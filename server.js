248722514f0fc54fbd61c81f317d5e6ce6c26e2c
Dakota O'Neil <oneilkody@gmail.com>
2021-05-21 13:17:11 -0500

    create rugby api

---
+++ server.js
@@ -0,0 +1,51 @@
+const express = require('express')
+const app = express()
+const cors = require('cors')
+const PORT = 4000
+
+app.use(cors())
+
+let ruggers = {
+    'jones': {
+        'firstName': 'Alun Wyn', 
+        'lastName': 'Jones',
+        'team': 'Wales', 
+        'position': 'Second Row'
+    },
+    'carter': {
+        'firstName': 'Dan', 
+        'lastName': 'Carter',
+        'team': 'NZ All Blacks', 
+        'position': 'Flyhalf'
+    },
+    'pocock': {
+        'firstName': 'David', 
+        'lastName': 'Pocock',
+        'team': 'Wallabies', 
+        'position': 'Flanker'
+    },
+    'oneil': {
+        'firstName': 'Dakota', 
+        'lastName': "O'Neil",
+        'team': 'Houston SaberCats', 
+        'position': 'Second Row'
+    }
+}
+
+app.get('/', (request, response) => {
+    response.sendFile(__dirname + '/index.html')
+})
+
+app.get('/api/ruggers/:ruggerName', (request, response) => {
+    const ruggerName = request.params.ruggerName.toLowerCase()
+    console.log(ruggerName)
+    if (ruggers[ruggerName]) {
+        response.json(ruggers[ruggerName])
+    }else{
+        response.json(ruggers['oneil'])
+    }
+})
+
+app.listen(PORT, () => {
+    console.log(`Listening on port ${PORT}`)
+})
